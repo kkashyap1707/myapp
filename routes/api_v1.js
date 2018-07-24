f4f3a6ebe9db3e202d2e16befafd73e0b7e393e8
keshav.kashyap <keshav.kashyap@tothenew.com>
2018-07-24 18:25:56 +0530

    comment yml file

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -5,9 +5,9 @@ const assert = require('assert');
 
 // Connection URL
 //const url = 'mongodb://mongo:27017';
-//const url = 'mongodb://52.77.223.186:27017';
-const url = 'mongodb://${process.env.MONGO_URI}:{process.env.MONGO_PORT}';
-
+const url = 'mongodb://52.77.223.186:27017';
+//const url = 'mongodb://${process.env.MONGO_URI}:{process.env.MONGO_PORT}';
+//console.log("MONGO_URL = "+process.env.MONGO_URI);
 
 // Database Name
 const dbName = 'myproject';
