273fe84d3e576a7598bc80bcd08ac2845103c288
keshav.kashyap <keshav.kashyap@tothenew.com>
2018-07-25 13:04:03 +0530

    Added hardcoded value

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -5,8 +5,8 @@ const assert = require('assert');
 
 // Connection URL
 //const url = 'mongodb://mongo:27017';
-const url = 'mongodb://52.77.223.186:27017';
-//const url = 'mongodb://${process.env.VIRTUAL_HOST}:${process.env.VIRTUAL_PORT}';
+//const url = 'mongodb://52.77.223.186:27017';
+const url = 'mongodb://${process.env.VIRTUAL_HOST}:${process.env.VIRTUAL_PORT}';
 //console.log("MONGO_URL = "+process.env.MONGO_URI);
 
 // Database Name
